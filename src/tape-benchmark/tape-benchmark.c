d086bd2822b02d9ce2e5d1e6a0d9f213bdfcd175
Clercin Guillaume <gclercin@intellique.com>
2011-11-15 11:37:58 +0100

    Update comment

--- src/tape-benchmark/tape-benchmark.c
+++ src/tape-benchmark/tape-benchmark.c
@@ -18,16 +18,11 @@
 *  GNU General Public License for more details.                             *
 *                                                                           *
 *  You should have received a copy of the GNU General Public License        *
-*  along with this program; if not, write to the Free Software              *
-*  Foundation, Inc., 51 Franklin Street, Fifth Floor,                       *
-*  Boston, MA  02110-1301, USA.                                             *
-*                                                                           *
-*  You should have received a copy of the GNU General Public License        *
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
 *                                                                           *
 *  -----------------------------------------------------------------------  *
 *  Copyright (C) 2011, Clercin guillaume <gclercin@intellique.com>          *
-*  Last modified: Thu, 10 Nov 2011 12:56:24 +0100                           *
+*  Last modified: Tue, 15 Nov 2011 11:37:51 +0100                           *
 \***************************************************************************/
 
 #include <errno.h>
