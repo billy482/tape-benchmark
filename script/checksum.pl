c7b842b08e24144f9dd6387fa6f6d8d6c444cd0f
Clercin Guillaume <gclercin@intellique.com>
2014-09-29 20:58:44 +0200

    Don't compare with previous checksum value

--- script/checksum.pl
+++ script/checksum.pl
@@ -6,14 +6,8 @@ use warnings;
 use IPC::Open2;
 
 # Try to read old checksum
-my $symbol      = shift @ARGV;
-my $filename    = shift @ARGV;
-my $oldChecksum = '';
-if ( open my $fd, '<', $filename ) {
-    my $line = <$fd>;
-    $oldChecksum = $1 if ( $line =~ /"(\w+)"/ );
-    close $fd;
-}
+my $symbol   = shift @ARGV;
+my $filename = shift @ARGV;
 
 my ( $fdin, $fdout );
 my $pid = open2( $fdout, $fdin, 'sha1sum', '-' ) or die "Can't exec sha1sum";
@@ -27,9 +21,7 @@ my $line       = <$fdout>;
 my $newCheckum = '';
 $newCheckum = $1 if ( $line =~ /^(\w+)\s/ );
 
-if ( $oldChecksum ne $newCheckum ) {
-    open my $fd, '>', $filename;
-    print $fd "#define TAPEBENCHMARK_SRCSUM \"$newCheckum\"\n";
-    close $fd;
-}
+open my $fd, '>', $filename;
+print $fd "#define TAPEBENCHMARK_SRCSUM \"$newCheckum\"\n";
+close $fd;
 
